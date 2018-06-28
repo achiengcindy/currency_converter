9234b43ae07c92af6e2751b9803e99d385deb1ca
achiengcindy <achiengcindy36@gmail.com>
2018-06-28 09:48:38 +0300

    conversion

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
+let currency = document.getElementById("currency");
+let from = document.getElementById("from");
+let to = document.getElementById("to");
+let final = document.getElementById("final");
+let convert = document.getElementById("convert");
+
+convert.onclick = (event) => {
+    let key = `${from.value}_${to.value}`;
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${key}&compact=ultra`)
+    .then((response) => response.json()) // Transform the data into json
+    .then((response) => {
+        final.value = response[key] * currency.value;
+    });
+};
 fetch('https://free.currencyconverterapi.com/api/v5/currencies')
 .then((response) => response.json()) // Transform the data into json
 .then((response) => {

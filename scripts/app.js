4c6ce875b83460cfa55d5d70ee68b29ad2518144
achiengcindy <achiengcindy36@gmail.com>
2018-06-28 09:46:23 +0300

    populatint from and to currency in html

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,10 @@
 fetch('https://free.currencyconverterapi.com/api/v5/currencies')
 .then((response) => response.json()) // Transform the data into json
 .then((response) => {
-    console.log('OK');
-     let from = document.getElementById("from");
-     Object.entries(response.results).forEach(([key, value]) => {
-         console.log(value);
-         let option = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
-         from.add(option);
-        });
+    Object.entries(response.results).forEach(([key, value]) => {
+        let fromOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
+        let toOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
+        from.add(fromOption);
+        to.add(toOption);
     });
+});

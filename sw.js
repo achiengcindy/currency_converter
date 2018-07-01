28efdefd5bb50ae577b23b9d1a39487083a53e88
Cindy Muga <achiengcindy36@gmail.com>
2018-07-01 22:47:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', event => {
         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
         'https://cdn.jsdelivr.net/npm/idb@2.1.3/lib/idb.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css',
-        //'https://free.currencyconverterapi.com/api/v5/currencies',
+        'https://free.currencyconverterapi.com/api/v5/currencies',
       ]);
     })
   );

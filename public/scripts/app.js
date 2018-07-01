b322d5de90e973c233ab8ea58eb9d656e09af193
achiengcindy <achiengcindy36@gmail.com>
2018-07-01 13:02:03 +0300

    OFFLINE CURRENCY CONVERSION RATES

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,58 +3,159 @@ let from = document.getElementById("from");
 let to = document.getElementById("to");
 let final = document.getElementById("final");
 let convert = document.getElementById("convert");
+let indexdb = idb.open('currency-convertion-rates-to-usd', 1, upgradeDB => {
+    // Note: we don't use 'break' in this switch statement,
+    // the fall-through behaviour is what we want.
+    switch (upgradeDB.oldVersion) {
+        case 0:
+            upgradeDB.createObjectStore('rates-to-usd-0');
+    }
+});
+
+const currencyRatesStore = {
+    get(key) {
+        return indexdb.then(db => {
+            return db.transaction('rates-to-usd-0')
+                .objectStore('rates-to-usd-0').get(key);
+        });
+    },
+    set(key, val) {
+        return indexdb.then(db => {
+            const tx = db.transaction('rates-to-usd-0', 'readwrite');
+            tx.objectStore('rates-to-usd-0').put(val, key);
+            return tx.complete;
+        });
+    },
+    delete(key) {
+        return indexdb.then(db => {
+            const tx = db.transaction('rates-to-usd-0', 'readwrite');
+            tx.objectStore('rates-to-usd-0').delete(key);
+            return tx.complete;
+        });
+    },
+    clear() {
+        return indexdb.then(db => {
+            const tx = db.transaction('rates-to-usd-0', 'readwrite');
+            tx.objectStore('rates-to-usd-0').clear();
+            return tx.complete;
+        });
+    },
+    keys() {
+        return indexdb.then(db => {
+            const tx = db.transaction('rates-to-usd-0');
+            const keys = [];
+            const store = tx.objectStore('rates-to-usd-0');
+
+            // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+            // openKeyCursor isn't supported by Safari, so we fall back
+            (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
+                if (!cursor) return;
+                keys.push(cursor.key);
+                cursor.continue();
+            });
+
+            return tx.complete.then(() => keys);
+        });
+    }
+};
 
 
 convert.onclick = (event) => {
     let key = `${from.value}_${to.value}`;
     fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${key}&compact=ultra`)
-    .then((response) => response.json()) // Transform the data into json
-    .then((response) => {
-        final.value = response[key] * currency.value;
-    });
+        .then(response => {
+            return response.json(); // Transform the data into json
+        })
+        .then(response => {
+            final.value = response[key] * currency.value;
+        }).catch(error => {
+            if (confirm(`Error loading conversion rate, do you want to use the pre-cached rates?`))
+                Promise.all([currencyRatesStore.get(from.value), currencyRatesStore.get(to.value)]).then(([from_conversion_rate, to_conversion_rate]) => {
+                    if (!from_conversion_rate) {
+                        alert(`${from.text} not yet cahed in IndexDB FOR offline use... `);
+                        return;
+                    }
+                    if (!to_conversion_rate) {
+                        alert(`${to.text} not yet cahed in IndexDB FOR offline use... `);
+                        return;
+                    }
+                    final.value = (+from_conversion_rate / +to_conversion_rate) * currency.value;
+                });
+        });
 };
 
+
 fetch('https://free.currencyconverterapi.com/api/v5/currencies')
-.then((response) => response.json()) // Transform the data into json
-.then((response) => {
-    Object.entries(response.results).forEach(([key, value]) => {
-        let fromOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
-        let toOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
-        from.add(fromOption);
-        to.add(toOption);
+    .then((response) => {
+        return response.json(); // Transform the data into json
+    })
+    .then(response => {
+        Object.entries(response.results).forEach(([key, value]) => {
+            let fromOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
+            let toOption = new Option(`${value.currencyName} (${value.currencySymbol})`, key);
+            from.add(fromOption);
+            to.add(toOption);
+        });
+        return response;
+    })
+    .then(response => {
+        var convertionRates = [];
+
+        //https://www.bennadel.com/blog/3201-exploring-recursive-promises-in-javascript.htm
+        var promise = Object.entries(response.results).reduce(
+            function reducer(promiseChain, currency) {
+                var nextLinkInChain = promiseChain.then(function () {
+                    [key, value] = currency;
+                    let key_USD = `${key}_USD`;
+                    return fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${key_USD}&compact=ultra`)
+                        .then(response => {
+                            return response.json(); // Transform the data into json
+                        })
+                        .then(response => {
+                            console.log(`successfully loaded ${response[key_USD]} => ${key}`);
+                            let usd_rate = {
+                                rate: response[key_USD],
+                                id: key
+                            };
+                            convertionRates.push(usd_rate);
+                            return usd_rate;
+                        }).catch(response => {
+                            console.log(`error loading ${key}`);
+                            let usd_rate = {
+                                rate: null,
+                                id: key
+                            };
+                            convertionRates.push(usd_rate);
+                            return usd_rate;
+                        })
+                });
+                return (nextLinkInChain);
+            },
+            Promise.resolve() // Start the promise chain.
+        );
+
+        return promise.then(function () {
+            return convertionRates;
+        });
+    })
+    .then((convertionRates) => {
+        console.log(convertionRates)
+        //save to indexedb
+        convertionRates.forEach(_rate => {
+            let savedRate = currencyRatesStore.get(_rate.id);
+            if (savedRate) {
+                if (_rate.rate && savedRate != _rate.rate)
+                    currencyRatesStore.set(_rate.id, _rate.rate);
+            } else
+                currencyRatesStore.set(_rate.id, _rate.rate);
+        });
     });
-
-//indexedb
-const dbPromise = idb.open('currency-converter-db', 1, (upgradeDb) => {
-    const currencyStore = upgradeDb.createObjectStore('currency-conversion');
-    
-    Object.entries(response.results).forEach(([key, value]) => {
-        currencyStore.put(`${value.currencyName} (${value.currencySymbol})`, key);
-        final.value = response[key] * currency.value;
-    });
-
-  });
-
-
-
-
-
-
-
-});
 
 //register service worker
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('/sw.js')
-             .then((cache) => {
-              console.log("Service Worker Registered"); 
-            });
-  }
-
-
-
-
-
-
-
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+        console.log("Service Worker Registered");
+    }).catch(() => {
+        console.log("Oooops! Service Worker Registered Failed");
+    });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,48 @@
-const cacheName = 'currency-converter-v1';
+const currencyCacheName = 'currency-static-v8';
 
-self.addEventListener('install', (event)=> {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) =>  {
+    caches.open(currencyCacheName).then(cache => {
       return cache.addAll([
-          '/',
-          '/css/styles.css',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-          '/scripts/app.js',
-          'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-          'https://free.currencyconverterapi.com/api/v5/currencies',
-          'https://cdn.jsdelivr.net/npm/idb@2.1.3/lib/idb.min.js',
-        ]);
-    }));
-});
-
-
-
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-      // Check cache but fall back to network
-      return response || fetch(event.request);
+        '/',
+        '/css/styles.css',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+        '/scripts/app.js',
+        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+        'https://free.currencyconverterapi.com/api/v5/currencies',
+        'https://cdn.jsdelivr.net/npm/idb@2.1.3/lib/idb.min.js',
+      ]);
     })
   );
 });
 
 
+//deleting old sw on activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('currency-') &&
+            cacheName != currencycacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+
+  );
+});
 
 
-
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // Fetch data from cache
+    caches.match(event.request).then((response) => {
+      // Check cache but fall back to network
+      return response || fetch(event.request);
+    })
+  );
+});
